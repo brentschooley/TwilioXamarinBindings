77e8eda42548f0a993195e5a501e668ac6542f0e
Denis Kornev <dkornev@aexsoft.com>
2016-06-27 14:46:13 +0300

    building projects for iOS simulators

--- source/Twilio.Common.iOS/libTwilioCommon.linkwith.cs
+++ source/Twilio.Common.iOS/libTwilioCommon.linkwith.cs
@@ -1,8 +1,8 @@
 using ObjCRuntime;
 
 [assembly: LinkWith ("libTwilioCommon.a", 
-    LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
+    LinkTarget.ArmV7 | LinkTarget.x86_64 | LinkTarget.ArmV7s| LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64 | LinkTarget.i386,
     Frameworks = "",
-    LinkerFlags = "-ObjC",
+    LinkerFlags = "-ObjC -lstdc++ -lc++ -lz -dead_strip",
     SmartLink = true, 
     ForceLoad = true)]
--- source/Twilio.Conversations.iOS/libTwilioConversationsClient.linkwith.cs
+++ source/Twilio.Conversations.iOS/libTwilioConversationsClient.linkwith.cs
@@ -1,9 +1,9 @@
 using ObjCRuntime;
 
 [assembly: LinkWith ("libTwilioConversationsClient.a", 
-    LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
+    LinkTarget.ArmV7 | LinkTarget.x86_64 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64 | LinkTarget.i386,
     Frameworks = "CoreTelephony VideoToolbox CoreMedia CoreVideo QuartzCore ImageIO CoreText AssetsLibrary MobileCoreServices CoreImage GLKit OpenGLES QuickLook UIKit Foundation CoreGraphics AudioToolbox CFNetwork SystemConfiguration AVFoundation",
-    LinkerFlags = "",
+    LinkerFlags = "-lstdc++ -lc++ -lz -dead_strip",
     IsCxx = true,
     SmartLink = true, 
     ForceLoad = true)]
